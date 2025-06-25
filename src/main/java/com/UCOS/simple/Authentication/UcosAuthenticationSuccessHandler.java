0d41173281fd1e39401c5ec86bc620508afee55e
Rick May <rick.may@technipfmc.com>
2025-06-25 14:52:43 -0700

    "Initial commit"

--- src/main/java/com/UCOS/simple/Authentication/UcosAuthenticationSuccessHandler.java
+++ src/main/java/com/UCOS/simple/Authentication/UcosAuthenticationSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.UCOS.simple.Authentication;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
--- src/main/java/com/UCOS/simple/Controller/UserController.java
+++ src/main/java/com/UCOS/simple/Controller/UserController.java
@@ -6,7 +6,6 @@ import com.UCOS.simple.Service.UserService;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
